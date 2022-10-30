da3b8798f3ad807ea889b2056d9221d5104749e8
Andrew Zimmerman <azimmerman@pga.com>
2022-10-30 17:49:16 -0400

    added foursquare markers and cleaned up code

--- map.js
+++ map.js
@@ -1,34 +1,89 @@
-//map object
 
+const myMap = {
+	coordinates: [],
+	businesses: [],
+	map: {},
+	markers: {},
 
-function buildmap() {
-	const pos = navigator.geolocation.getCurrentPosition(resolve, reject)
+	buildMap() {
+		this.map = L.map('map', {
+		center: this.coordinates,
+		zoom: 11,
+		});
 
-    function resolve(pos) {
-        let lat = pos.coords.latitude
-        let long = pos.coords.longitude
-        let map = L.map('map').setView([lat, long], 13);
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution:
+			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		minZoom: '12',
+		}).addTo(this.map)
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+		const marker = L.marker(this.coordinates)
+		marker
+		.addTo(this.map)
+		.bindPopup('<p1><b>You are here</b><br></p1>')
+		.openPopup()
+	},
 
-        const marker = L.marker([lat, long]);
-            marker.addTo(map).bindPopup('<p1><b>Current Location</b></p1>').openPopup();
-        
-    }
-
-    function reject() {
-        console.alert('Your current location cannot be found.')
-    }
+	addMarkers() {
+		for (var i = 0; i < this.businesses.length; i++) {
+		this.markers = L.marker([
+			this.businesses[i].lat,
+			this.businesses[i].long,
+		])
+			.bindPopup(`<p1>${this.businesses[i].name}</p1>`)
+			.addTo(this.map)
+		}
+	},
 }
 
-buildmap()
+async function getCoords(){
+	const pos = await new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject)
+	});
+	return [pos.coords.latitude, pos.coords.longitude]
+}
 
+async function getFoursquare(business) {
+	const options = {
+		method: 'GET',
+		headers: {
+		Accept: 'application/json',
+		Authorization: 'fsq3jupWFh+dkUyzVcLDhr4H2Ofj3SO1pMame4DtcOVszl0='
+		}
+	}
+	let limit = 5
+	let lat = myMap.coordinates[0]
+	let lon = myMap.coordinates[1]
+	let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+	let data = await response.text()
+	let parsedData = JSON.parse(data)
+	let businesses = parsedData.results
+	return businesses
+}
 
-//listeners for submit button
-document.getElementById('submit').addEventListener('click',function() {
-    let place = document.getElementById('selectPlace').value
-    console.log(place)
+function processBusinesses(data) {
+	let businesses = data.map((element) => {
+		let location = {
+			name: element.name,
+			lat: element.geocodes.main.latitude,
+			long: element.geocodes.main.longitude
+		};
+		return location
+	})
+	return businesses
+}
+
+// event handlers
+window.onload = async () => {
+	const coords = await getCoords()
+	myMap.coordinates = coords
+	myMap.buildMap()
+}
+
+document.getElementById('submit').addEventListener('click', async (event) => {
+	event.preventDefault()
+	let business = document.getElementById('business').value
+	let data = await getFoursquare(business)
+	myMap.businesses = processBusinesses(data)
+	myMap.addMarkers()
 })
